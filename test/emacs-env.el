d39c011b0aa64bd5f047344fff3c57c560eba53c
Alex Branham <branham@utexas.edu>
2018-06-07 10:50:10 -0500

    Use archlinux docker image for gitlab CI

--- test/emacs-env.el
+++ test/emacs-env.el
@@ -3,11 +3,8 @@
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t))
 (package-initialize)
 
 (package-refresh-contents)
@@ -16,5 +13,3 @@
   (package-install 'helm))
 (unless (package-installed-p 'password-store)
   (package-install 'password-store))
-(unless (package-installed-p 'auth-password-store)
-  (package-install 'auth-source-pass))
